3b7fe53a5f61138ea3d2e56d475bb3c85e9f49ae
Michael Travers <michael.travers@udc.edu>
2022-09-06 09:31:05 -0400

    calenderpage

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragandDrop, DragAndDrop } from '@syncfusion/ej2-react-schedule'
+import { scheduleData } from '../data/dummy'
+import { Header } from '../components'
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars'
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="App" title='Calender'/>
+      <ScheduleComponent
+      height="650px"
+      eventSettings={{dataSource: scheduleData}}
+      selectedDate= {new Date(2021, 0, 10)}>
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}/>
+      </ScheduleComponent>
+      </div>
   )
 }
 
