3ee5c74916907ff0384d25e129ec521c01d43d07
Michael Travers <michael.travers@udc.edu>
2022-09-07 11:33:02 -0400

    settings button opens a close

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ const App = () => {
               <Navbar />
             </div>
             <div>
-              <ThemeSettings />
+              {themeSettings && <ThemeSettings />}
 
               <Routes>
                 {/* dashboard  */}
